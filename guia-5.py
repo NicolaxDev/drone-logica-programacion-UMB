92b923dc6adea77cbeb722cf12e6a2c71ceba8fb
NicolaxDev <146976350+NicolaxDev@users.noreply.github.com>
2025-10-28 11:25:23 -0500

    power up has been added

--- guia-5.py
+++ guia-5.py
@@ -1,25 +1,35 @@
+"""
+    Decidi solo manejar una clase dentro de mi codigo, el dron, con el metodo principal que es el de permitir el movimiento del drone, este tambien verifica
+    las colisiones del drone con los diferentes objetivos como lo puede ser el muro (#), el destino o meta (T) y un espacio en blanco que permite que 
+    avance el drone. todo como se estipulaba en las guias 4 y 5.
+
+    mediante la logica de las colisiones se pueden agregar varios objetos de mapa, por ejemplo un power up que permita al drone avanzar 2 veces mas rapido 
+    solamente cuando se avanza hacia adelante.
+"""
+
 map = [
     ["D"," "," "," ","#"," "," "," "," "," "," "," "," "," "," "," "," "," "," ","#"],
     ["#"," ","#"," ","#"," "," "," "," "," ","#"," "," "," "," ","#"," "," "," ","#"],
-    ["#"," ","#"," "," "," "," ","#"," "," "," "," "," ","#"," ","#"," "," "," ","#"],
+    ["#"," ","#"," "," "," "," ","#"," "," "," "," "," ","#"," ","#"," ","V"," ","#"],
     ["#"," ","#"," ","#"," "," ","#"," ","#"," "," "," ","#"," "," "," ","#"," ","#"],
-    ["#"," "," "," ","#"," "," "," "," ","#"," "," "," ","#"," "," ","#"," "," ","#"],
+    ["#"," "," "," ","#"," "," "," "," ","#"," ","V"," ","#"," "," ","#"," "," ","#"],
     ["#"," ","#"," "," "," ","#"," "," "," "," ","#"," "," "," ","#"," "," "," ","#"],
-    ["#"," ","#"," "," ","#"," "," ","#"," "," "," "," "," ","#"," "," "," "," ","#"],
+    ["#"," ","#"," "," ","#"," "," ","#"," "," "," "," ","V","#"," "," "," "," ","#"],
     ["#"," "," "," ","#"," "," ","#"," ","#"," ","#"," "," "," ","#"," ","#"," ","#"],
-    ["#"," ","#"," "," "," "," "," ","#"," "," ","#"," "," "," "," "," ","#"," ","#"],
+    ["#"," ","#"," "," "," ","V"," ","#"," "," ","#"," "," "," "," "," ","#"," ","#"],
     ["#"," ","#"," "," ","#"," ","#"," "," "," "," ","#"," ","#"," "," "," "," ","#"],
-    ["#"," ","#"," "," ","#"," ","#"," ","#"," "," ","#"," ","#"," "," "," "," ","#"],
+    ["#"," ","#"," "," ","#"," ","#"," ","#"," "," ","#"," ","#"," ","V"," "," ","#"],
     ["#"," "," "," "," ","#"," "," ","#"," "," ","#"," "," "," ","#"," "," "," ","#"],
     ["#"," ","#"," ","#"," "," ","#"," ","#"," ","#"," ","#"," ","#"," ","#"," ","#"],
     ["#"," ","#"," "," "," ","#"," "," ","#"," "," ","#"," "," "," ","#"," "," ","#"],
     ["#"," ","#"," ","#"," "," "," "," "," ","#"," ","#"," "," "," "," ","#"," ","#"],
-    ["#"," "," "," ","#"," ","#"," "," ","#"," "," "," "," ","#"," ","#"," "," ","#"],
+    ["#"," "," "," ","#"," ","#"," ","V","#"," "," "," "," ","#"," ","#"," "," ","#"],
     ["#"," ","#"," "," "," ","#"," "," "," "," "," ","#"," "," "," ","#"," "," ","#"],
     ["#"," ","#"," "," "," "," "," ","#"," ","#"," "," ","#"," "," "," "," "," ","#"],
-    ["#"," ","#"," "," "," "," "," "," "," ","#"," "," "," "," "," "," "," ","T","#"],
+    ["#"," ","#"," ","V"," "," "," "," "," ","#"," "," ","V"," "," "," "," ","T","#"],
     ["#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#","#"]
 ]
+
 
 
 class Drone:
@@ -27,6 +37,7 @@ class Drone:
         self.mapa = mapa
         self.posX = posX
         self.posY = posY
+        self.power_up = False
         print(f"Drone creado en x: {posX}, y: {posY}")
 
     def mostrar_mapa(self):
@@ -46,14 +57,21 @@ class Drone:
                 break
 
             dy, dx = 0, 0
+            aumento = 0
+
+            #Up
             if tecla == "w":
                 dy = -1
+                if self.power_up:
+                    dy *= 2
+                    self.power_up = False
+                    print("💨 Power-up usado: avance doble hacia adelante.")
             elif tecla == "s":
-                dy = 1
+                dy = 1 + aumento
             elif tecla == "a":
                 dx = -1
             elif tecla == "d":
-                dx = 1
+                dx = 1 + aumento
             else:
                 print("Movimiento no válido.")
                 continue
@@ -76,6 +94,13 @@ class Drone:
                 self.mostrar_mapa()
                 print("¡Llegaste al objetivo!")
                 break
+            elif destino == "V":
+                print("Power-up obtenido")
+                self.power_up = True
+                self.mapa[self.posY][self.posX] = " "
+                self.mapa[ny][nx] = "D"
+                self.posY = ny
+                self.posX = nx
             else:
                 self.mapa[self.posY][self.posX] = " "
                 self.mapa[ny][nx] = "D"
